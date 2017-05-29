17ffdf843fbecb5f2349928ae7406ca058dd0aca
DeepFriedSeagull <cedric.gautier.work@gmail.com>
2017-05-29 11:26:49 +0200

    Example de fichier avec appel de fonctions

---
+++ example.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def is_it_time_to_eat():
+	print( datetime.now().isoformat() )
+	if (12 < datetime.now().hour and 14 > datetime.now().hour ) :
+		return True
+	else:
+		return False
+
+def main():
+	print( is_it_time_to_eat() )
+
+mavariable = 'Toto'
+
+if __name__ == "__main__":
+	# appeller ma function principalle
+	main()
